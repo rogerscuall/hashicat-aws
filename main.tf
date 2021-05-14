fbb8eea14be2ea159fe255108992378c3f54c4f7
Roger Gomez <rgomez@presidio.com>
2021-05-14 15:29:24 -0400

    permit ssh

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.subnet_prefix]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
